4937ab64c0d96de86f15cb09e427b826a01f12d7
Nicklas Carlsson <ncasv92@hotmail.com>
2025-05-04 15:01:07 +0200

    Create support function get_all_books_list for katalog page.

--- tests/pages/katalog_page.py
+++ tests/pages/katalog_page.py
@@ -7,16 +7,39 @@ class KatalogPage:
         self.booklist_div = page.locator("div.catalog")
         self.a_book = page.get_by_text("❤️\"Hur man tappar bort sin TV")
 
-    def get_all_books(self):
+    def get_all_books_list(self):
         books = self.page.locator('.book').all()
-        return books
+        all_books_data=[]
+        for book in books:
+
+            book_text = book.inner_text()
+            book_text = book_text.replace('❤️','').strip()#Remove heart
+
+            titel =book_text[0:book_text.rfind('"')+1]
+            author = book_text[book_text.rfind('"') + 1:].strip(", ")
+
+            #Find out if the book is favorite and also test id
+            star_element = book.locator('.star')
+            book_test_id = star_element.get_attribute('data-testid')
+            star_classes = star_element.get_attribute('class')
+            is_favorite = 'selected' in star_classes
+
+            all_books_data.append({
+                "book_test_id": book_test_id,
+                "titel": titel,
+                "author": author,
+                "is_favorite": is_favorite
+            })
+        return all_books_data
 
     def a_book_is_visible(self):
         return self.a_book.is_visible()
 
     def count_books(self):
-        return self.booklist_div.locator("div.book").count()
+        books=self.get_all_books_list()
+        return books.count()
 
     def book_displays(self, titel):
         book = self.page.get_by_test_id(f"star-{titel}")
         expect(book).to_be_visible()
+
